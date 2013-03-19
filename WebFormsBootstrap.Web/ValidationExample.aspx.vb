c0267c83fb94c8de2cbba226775efaaa7170fbcf
Jay Douglass <jay.douglass@gmail.com>
2013-03-19 15:35:29 -0500

    Reformatted example

--- WebFormsBootstrap.Web/ValidationExample.aspx.vb
+++ WebFormsBootstrap.Web/ValidationExample.aspx.vb
@@ -8,7 +8,10 @@ Public Class ValidationExample
     End Sub
 
     Protected Sub btnValidate_Click()
-        txtAge.Validate().Required("Enter the age").IsInteger("Please enter a valid age").ApplyRule(AddressOf IsNotOld, "Too old")
+        txtAge.Validate() _
+            .Required("Enter the age") _
+            .IsInteger("Please enter a valid age") _
+            .ApplyRule(AddressOf IsNotOld, "Too old")
         txtFirstName.Validate().Required("Enter the first name").Length("first name", 7)
         txtLastName.Validate().Required("Enter the last name").Length("last name", 5)
         txtStartDate.Validate().Required("Enter the start date").IsDate("Enter a valid start date")
